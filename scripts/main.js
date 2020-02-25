dc87630d4124432a25b68eb749b533ccf72cc125
Rathor <travis.l.dalton@gmail.com>
2020-02-25 10:40:14 -0700

    tutorial updates to website for button and h1 change. Also added a 'change' user button that allows you to prompt user for a name and update h1 to reflect the name.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = "Hello World!!";
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/blue.jpg') {
+        myImage.setAttribute ('src', 'images/green.jpg');
+    } else {
+        myImage.setAttribute ('src', 'images/blue.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt("Please enter your name. ");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "MTG is cool, " + myName;
+
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = "MTG is cool, " + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+    
+}
